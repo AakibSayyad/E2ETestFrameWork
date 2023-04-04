03116ff103937aa70a5f3a7892dbd2f0e14422a4
Aakib Sayyad <aakib.sayyad@ibm.com>
2023-04-04 13:37:37 +0530

    ads

---
+++ src/main/java/testRunner.java
@@ -0,0 +1,9 @@
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/features/Test1.feature", glue = "StepDefinations", monochrome = true)
+public class testRunner extends AbstractTestNGCucumberTests {
+
+}
