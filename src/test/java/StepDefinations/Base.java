891012da52d03261686e84e44737a2c0edabc779
Aakib Sayyad <aakib.sayyad@ibm.com>
2023-04-20 22:26:14 +0530

    allure

--- src/test/java/StepDefinations/Base.java
+++ src/test/java/StepDefinations/Base.java
@@ -1,4 +1,5 @@
 package StepDefinations;
+
 import java.util.*;
 import java.io.*;
 import java.time.Duration;
@@ -11,31 +12,34 @@ import org.openqa.selenium.edge.EdgeOptions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-public  class Base {
+
+public class Base {
 	public static WebDriver driver;
 	public static Properties prop;
 	public static EdgeOptions options;
-	
+
 	@Before
-	public static  void setup() throws IOException {
-	prop = new Properties();
-	FileInputStream fis  = new FileInputStream(System.getProperty("user.dir")+"/src/test/java/StepDefinations/prop.properties");
-	prop.load(fis);
-	String browser = prop.getProperty("browser");
-	if(browser.equals("chrome")) {
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	public static void setup() throws IOException {
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "/src/test/java/StepDefinations/prop.properties");
+		prop.load(fis);
+		String browser = prop.getProperty("browser");
+		if (browser.equals("chrome")) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+		if (browser.equals("edge")) {
+			driver = new EdgeDriver();
+
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
 	}
-	if(browser.equals("edge")) {
-		driver = new EdgeDriver();
-		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	}
-	}
+
 	@After
 	public void close() {
-		driver.close();
+		driver.quit();
 	}
-	}
+}
--- src/test/java/TestRunner/testRunner.java
+++ src/test/java/TestRunner/testRunner.java
@@ -3,7 +3,7 @@ package TestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features/amazonSignInTest.feature", glue = "StepDefinations", monochrome = true,dryRun=false)
+@CucumberOptions(features = "src/test/java/features/Amazon.feature", glue = "StepDefinations", monochrome = true,dryRun=false)
 public class testRunner extends AbstractTestNGCucumberTests {
 
 }
