1ebb5a26e7d6b825b191c830f377d3f904d523f1
Aakib Sayyad <aakib.sayyad@ibm.com>
2023-05-03 15:15:18 +0530

    dasdasddsasda

--- src/test/java/StepDefinations/CheckoutTest.java
+++ src/test/java/StepDefinations/CheckoutTest.java
@@ -18,7 +18,7 @@ public class CheckoutTest {
 		Base.driver.findElement(By.id("twotabsearchtextbox")).sendKeys(productname);
 		Base.driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]")).click();
 		JavascriptExecutor js = (JavascriptExecutor) Base.driver;
-		js.executeScript("window.scrollBy(0,250)", "");
+		js.executeScript("window.scrollBy(0,350)", "");
 		String xpath ="//span[normalize-space()='CE 3 Lite 5G (Chromatic Gray, 8GB RAM, 128GB Storage)']";
 		
 		
